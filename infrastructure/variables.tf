fbda701b01e58b0eaa2e4581dddbc0f926b77b5d
schumi0 <carlaugustramstad@gmail.com>
2024-11-19 23:03:38 +0000

    cool2

---
+++ infrastructure/variables.tf
@@ -0,0 +1,10 @@
+
+variable "prefix" {
+  description = "Prefix to be added to variable and function names"
+  type        = string
+}
+
+variable "notification_email" {
+  description = "Email for notifications cloudwatch"
+  type        = string
+}
