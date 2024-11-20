5ee92e505276f1165db338b6b93831139f09ffca
schumi0 <carlaugustramstad@gmail.com>
2024-11-20 00:17:23 +0000

    coldaf9

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,13 +34,13 @@ resource "aws_sqs_queue" "imggen_que" {
 # IAM Role for Lambda
 resource "aws_iam_role" "lambda_exec_role" {
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    Version = "2012-10-17",
+    Statement = [
       {
-        "Action": "sts:AssumeRole",
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com"
         }
       }
     ]
